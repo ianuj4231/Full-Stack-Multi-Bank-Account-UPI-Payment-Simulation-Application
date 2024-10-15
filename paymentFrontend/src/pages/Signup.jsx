6ba55c14a62b021c3397f1852a58c9a8913014dd
I. Anuj <134675919+ianuj4231@users.noreply.github.com>
2024-10-15 19:16:14 +0530

    Update Signup.jsx
    .

--- paymentFrontend/src/pages/Signup.jsx
+++ paymentFrontend/src/pages/Signup.jsx
@@ -1,54 +1,42 @@
 // import '../App.css'
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { listofbanks } from '../bankData';
 import { toast } from 'react-toastify';
-import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-let changes = false;
+
 export default function Signup() {
-    console.log("cllaed")
+    console.log("called");
     const navigate = useNavigate();
 
     const [isSigninClicked, setIsSigninClicked] = useState(false);
-    const [isAlert, setIsAlert] = useState(false);
     const [isSignupClicked, setIsSignupClicked] = useState(true);
-    const [signinFormData, setSigninFormData] = useState({ username: "", password: "" })
+    const [signinFormData, setSigninFormData] = useState({ username: "", password: "" });
     const [signupFormData, setSignupFormData] = useState({
         username: "",
         password: "",
         firstname: "",
         lastname: "",
         bankId: ""
-    })
-    useEffect(() => {
-        let timerId;
-        if (changes) {
+    });
 
-            timerId = setTimeout(() => {
-                changes = false
-                setIsAlert((prev) => !prev)
-
-            }, 2000);
-        }
-        return () => clearTimeout(timerId)
-    }, [isAlert])
     function isSigninClickedFn() {
         setIsSigninClicked(true);
         setIsSignupClicked(false);
     }
+
     function isSignupClickedFn() {
         setIsSignupClicked(true);
         setIsSigninClicked(false);
     }
+
     function handleChangeSignup(e) {
-        setSignupFormData({ ...signupFormData, [e.target.name]: e.target.value })
+        setSignupFormData({ ...signupFormData, [e.target.name]: e.target.value });
     }
 
     function handleChangeSignin(e) {
         const { name, value } = e.target;
-        setSigninFormData({ ...signinFormData, [name]: value })
+        setSigninFormData({ ...signinFormData, [name]: value });
     }
-
 
     function handleSubmitSignup(e) {
         e.preventDefault();
@@ -61,27 +49,23 @@ export default function Signup() {
             },
             body: JSON.stringify(signupFormData)
         })
-
             .then((response) => {
                 if (response.ok) {
                     console.log("Signup successful:", response);
-
                     return response.json();
                 } else {
                     return response.json().then(errorData => {
                         throw new Error(errorData.message || 'An error occurred');
-                    }
-                    );
+                    });
                 }
             })
             .then(data => {
                 console.log("Signup successful:", data);
-                // localStorage.setItem("username", username);
                 toast.success('Signup successful!');
-                localStorage.setItem("token", data.token)
+                localStorage.setItem("token", data.token);
                 let tokenxx = localStorage.getItem("token");
                 console.log("tokenxx ", tokenxx);
-                navigate("/customerDashboard")
+                navigate("/customerDashboard");
             })
             .catch(err => {
                 console.log(err.message);
@@ -89,64 +73,30 @@ export default function Signup() {
             });
     }
 
-    /*
-     async function handleSubmitSignup(e) {
-         e.preventDefault();
-     
-         try {
-             const response = await axios.post("http://localhost:3000/api/v1/user/signup", signupFormData);
-             console.log(response.data);
-             toast.success('Signup successful!');
-         } catch (error) {
-             if (error.response && error.response.data) {
-                 console.error("Error response data:", error.response.data.message);
-                 toast.error(`Error: ${error.response.data.message}`);
-             } else {
-                 console.error("Error:", error.message);
-                 toast.error(`Error: ${error.message}`);
-             }
-         }
-     }
- 
-     */
-    function handleForm(e) {
-        e.preventDefault()
-        const formData = new FormData(e.target);
-        console.log(formData)
-        if (formData.get('username') == '' || formData.get('password') == '') {
-            changes = true
-            setIsAlert(true)
-            return
-        }
-        else {
-            handleSubmitSignin()
-        }
-    }
-    async function handleSubmitSignin() {
+    async function handleSubmitSignin(e) {
+        e.preventDefault();
         console.log(signinFormData);
         try {
-            const response = await axios.post("http://localhost:3000/api/v1/user/signin", signinFormData);
-            console.log(response.data);
-            toast.success('Signin successful!');
-            localStorage.setItem("token", response.data.token)
-            let tokenxx = localStorage.getItem("token");
-            console.log("tokenxx ", tokenxx);
-
-            localStorage.setItem("username", signinFormData.username)
-
-            navigate("/customerDashboard")
-
-            // localStorage.setItem("username", username);
-
-        }
-        catch (error) {
-            if (error.response && error.response.data) {
-                console.error("Error response data:", error.response.data.message);
-                toast.error(`Error: ${error.response.data.message}`);
+            const response = await fetch("http://localhost:3000/api/v1/user/signin", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(signinFormData)
+            });
+            const data = await response.json();
+            if (response.ok) {
+                console.log(data);
+                toast.success('Signin successful!');
+                localStorage.setItem("token", data.token);
+                localStorage.setItem("username", signinFormData.username);
+                navigate("/customerDashboard");
             } else {
-                console.error("Error:", error.message);
-                toast.error(`Error: ${error.message}`);
+                throw new Error(data.message || 'An error occurred');
             }
+        } catch (error) {
+            console.error("Error:", error.message);
+            toast.error(`Error: ${error.message}`);
         }
     }
 
@@ -156,10 +106,8 @@ export default function Signup() {
                 Full-Stack Multi-Bank Account UPI Payment Simulation Application
             </div>
             <br />
-
-
             <div>
-                <span >
+                <span>
                     <button style={{ fontSize: "16.5px" }} onClick={isSignupClickedFn}>--signup-- </button>
                 </span>
                 <span>
@@ -168,65 +116,40 @@ export default function Signup() {
             </div>
 
             <div>
-                {isSignupClicked &&
-                    <div>
-                        <br></br>
-                        <br></br>
-                        <input type="text" onChange={handleChangeSignup} name="username" value={signupFormData.username} placeholder='enter username' />
-                        <br></br>
-                        <br></br>
-
-                        <input type="password" onChange={handleChangeSignup} name="password" value={signupFormData.password} placeholder='enter password' />
-                        <br></br>
-                        <br></br>
-
-                        <input type="text" onChange={handleChangeSignup} name="firstname" value={signupFormData.firstname} placeholder='enter firstname' />
-                        <br></br>
-                        <br></br>
-
-                        <input type="text" onChange={handleChangeSignup} name="lastname" value={signupFormData.lastname} placeholder='enter lastname' />
-                        <br></br>
-                        <br></br>
-
+                {isSignupClicked && (
+                    <form onSubmit={handleSubmitSignup}>
+                        <br />
+                        <input type="text" onChange={handleChangeSignup} name="username" value={signupFormData.username} placeholder='enter username' required />
+                        <br /><br />
+                        <input type="password" onChange={handleChangeSignup} name="password" value={signupFormData.password} placeholder='enter password' required />
+                        <br /><br />
+                        <input type="text" onChange={handleChangeSignup} name="firstname" value={signupFormData.firstname} placeholder='enter firstname' required />
+                        <br /><br />
+                        <input type="text" onChange={handleChangeSignup} name="lastname" value={signupFormData.lastname} placeholder='enter lastname' required />
+                        <br /><br />
                         <select onChange={handleChangeSignup} name="bankId" value={signupFormData.bankId} required>
                             <option value="" disabled>Select a bank</option>
-                            {
-                                listofbanks.map((x, index) => (
-                                    <option key={index} value={x.bankId}>
-                                        -- {x.bankname}--
-                                    </option>
-                                ))
-                            }
+                            {listofbanks.map((x, index) => (
+                                <option key={index} value={x.bankId}>
+                                    -- {x.bankname}--
+                                </option>
+                            ))}
                         </select>
+                        <br /><br />
+                        <button type="submit">sign up</button>
+                    </form>
+                )}
 
-
-                        <br></br>
-                        <br></br>
-
-                        <button onClick={handleSubmitSignup}>  sign up  </button>
-                    </div>
-
-                }
-
-                <div>
-                    {
-                        isSigninClicked && <div>
-                            <form action="" onSubmit={handleForm}>
-                                <br></br>
-                                <br></br>
-                                <input type="email" required onChange={handleChangeSignin} name="username" value={signinFormData.username} placeholder='enter username' />
-                                <br></br>
-                                <br></br>
-
-                                <input type="password" onChange={handleChangeSignin} name="password" value={signinFormData.password} placeholder='enter password' />
-                                <br></br>
-                                <br></br>
-                                <button type='submit' > signin </button>
-                            </form>
-                            {isAlert && <h1> Please fill all details</h1>}
-                        </div>
-                    }
-                </div>
+                {isSigninClicked && (
+                    <form onSubmit={handleSubmitSignin}>
+                        <br />
+                        <input type="email" required onChange={handleChangeSignin} name="username" value={signinFormData.username} placeholder='enter username' />
+                        <br /><br />
+                        <input type="password" onChange={handleChangeSignin} name="password" value={signinFormData.password} placeholder='enter password' required />
+                        <br /><br />
+                        <button type='submit'>signin</button>
+                    </form>
+                )}
             </div>
         </div>
     );
